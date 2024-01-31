99d260f98f4f4feb68b98a38c29a9813154e0589
Anas <wg12181218@gmail.com>
2024-01-31 17:30:35 +0900

    01.31 21736 풀이

--- 모르고리즘/3주차/21736_헌내기.py
+++ 모르고리즘/3주차/21736_헌내기.py
@@ -12,7 +12,6 @@ Queue = deque()
 
 for i in range(N):
   campus.append(list(map(str, sys.stdin.readline().strip())))
-  
   for j in range(len(campus[i])):
     if campus[i][j] == 'I':
       Queue.append([i,j])
@@ -21,4 +20,20 @@ for i in range(N):
 answer = 0
 
 # BFS -> 
-# while Queue:
+while Queue:
+  for _ in range(len(Queue)):
+    x, y = Queue.popleft()
+    for i in range(4):
+      nx,ny = x + dx[i], y + dy[i]
+      
+      if 0 <= nx < N and 0 <= ny < M and ch[nx][ny] == 0 and campus[nx][ny] != 'X':
+        if campus[nx][ny] == 'P':
+          answer += 1
+        ch[nx][ny] = 1
+        Queue.append([nx, ny])
+
+if answer:
+  print(answer)
+
+else:
+  print('TT')
