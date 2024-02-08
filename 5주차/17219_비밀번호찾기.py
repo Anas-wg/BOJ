5aa1f9f4a06dce6ad27837813540f5b584267c9f
Anas <wg12181218@gmail.com>
2024-02-08 15:19:49 +0900

    02.08 update

--- 5주차/17219_비밀번호찾기.py
+++ 5주차/17219_비밀번호찾기.py
@@ -5,23 +5,33 @@ import sys
 N, M = map(int,sys.stdin.readline().strip().split())
 
 ## dict 형식으로 받고,
-## 뒤에서 M번째 줄부터 일치하는 비번 찾아주기
-box = {}
-page_list = []
-pw_list = []
+# box = {}
+# page_list = []
+# pw_list = []
 
-for i in range(N):
-  id, pw = sys.stdin.readline().strip().split()
-  box[id] = pw
+# for i in range(N):
+#   id, pw = sys.stdin.readline().strip().split()
+#   box[id] = pw
 
-for j in range(-M, 0):
-  page = sys.stdin.readline().strip()
-  page_list.append(page)
+# for j in range(-M, 0):
+#   page = sys.stdin.readline().strip()
+#   page_list.append(page)
 
-for k in range(len(page_list)):
-  pw = box.get(page_list[k])
-  pw_list.append(pw)
+# for k in range(len(page_list)):
+#   pw = box.get(page_list[k])
+#   pw_list.append(pw)
 
 
-for l in range(len(pw_list)):
-  print(pw_list[l])
+# for l in range(len(pw_list)):
+#   print(pw_list[l])
+
+m, n = map(int, input().split())
+
+find ={}
+for i in range(m):
+    site, pw = input().split()
+    find[site] = pw
+
+for j in range(n):
+    f_site = input()
+    print(find[f_site])
