969189b0f884b278ed8c79e2ae36cd208eeb9982
Anas <wg12181218@gmail.com>
2024-02-10 19:45:55 +0900

    02.10 풀이 update

--- 06_심화1/1157_단어공부.py
+++ 06_심화1/1157_단어공부.py
@@ -1,11 +1,16 @@
+# 대소문자 구분 없으니 대문자 화
 word = input().upper()
+# 집합으로 만들어서 중복 X, 
+# set 함수는 mutation X
 word_list = list(set(word))
 
 cnt = []
 for i in word_list:
+  # count 메서드 -> 문자열내 특정 문자열 개수 구함
   count = word.count
   cnt.append(count(i))
 
+print(cnt)
 if cnt.count(max(cnt)) > 1:
   print("?")
 
